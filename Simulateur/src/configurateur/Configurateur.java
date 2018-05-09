f93255c6e1d2ab3e21994ef635f6da830d0cba1f
Bap <bleuzeb@etu.univ-grenoble-alpes.fr>
2018-05-09 09:47:06 +0200

    V0

---
+++ Simulateur/src/configurateur/Configurateur.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configurateur;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author bleuzeb
+ */
+public class Configurateur {
+    
+    private final List<String> nomsObjets = new ArrayList<>(128) ;
+
+    public List<String> getNomsObjets() {
+	return nomsObjets;
+    }
+    
+    public void lireObjets (String nomFichierEntree) {
+	Scanner scanner ;
+	try {
+	    scanner = new Scanner(new File(nomFichierEntree)) ;
+	    scanner.useDelimiter("\\s*\n+");
+	    while (scanner.hasNext()) {
+		String objet = scanner.next() ;
+		if (objet.matches("\\w+"))
+		    nomsObjets.add(objet) ;
+		else {
+		    System.err.println("L'objet " + "\"" + objet + "\" a un nom incorrect.") ;
+		    scanner.close();
+		    System.exit(2);
+		}
+	    }
+	    System.out.println(nomsObjets);
+	    scanner.close();
+	} catch (FileNotFoundException ex) {
+	    System.err.println("Le fichier de configuration " + nomFichierEntree + " n'existe pas");
+	    System.exit(1);
+	}
+	
+    }
+    
+}
---
+++ Simulateur/src/simulateur/Simulateur.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulateur;
+
+import configurateur.Configurateur;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author bleuzeb
+ */
+public class Simulateur {
+    
+    private final Configurateur configurateur ;
+
+    public Simulateur(Configurateur configurateur) {
+	this.configurateur = configurateur;
+    }
+
+    public Configurateur getConfigurateur() {
+	return configurateur;
+    }
+    
+    private void lireFormatOneEventByLine (String nomFichierEntree) {
+	Scanner scanner ;
+	try {
+	    scanner = new Scanner(new File(nomFichierEntree)) ;
+	    Pattern pattern = Pattern.compile("\\d\\d:\\d\\d:\\d\\d\\s*;\\s*\\w+\\s*;\\s*\\w+");
+	    String [] ligneDecoupee ;
+	    int l = 0 ;
+	    while (scanner.hasNext()) {
+		scanner.skip("\\s*") ;
+		String ligne = scanner.findInLine(pattern) ;
+		l++ ;
+		if (ligne != null) {
+		    ligneDecoupee = ligne.split("\\s*;\\s*") ;
+		    System.out.println(java.util.Arrays.toString(ligneDecoupee));
+		    scanner.nextLine();
+		}
+		else {
+		    System.err.println("La ligne n°" + l + " " + ligne + " ne respecte pas le format \"One-Event-By-Line\".") ;
+		    scanner.close();
+		    System.exit(2);
+		}
+	    }
+	    //System.out.println(nomsObjets);
+	    scanner.close();
+	} catch (FileNotFoundException ex) {
+	    System.err.println("Le fichier de configuration " + nomFichierEntree + " n'existe pas");
+	    System.exit(1);
+	}
+    }
+    
+    private void ecrireFormatCSV (String nomFichierSortie) {
+	BufferedWriter bufferedWriter ;
+	try {
+	    bufferedWriter = new BufferedWriter(new FileWriter(nomFichierSortie));
+	    bufferedWriter.write("timestamp; ") ;
+	    List<String> nomsObjets = configurateur.getNomsObjets() ;
+	    int i = 0 ;
+	    for (; i < nomsObjets.size()-1; i++) {
+		bufferedWriter.write(nomsObjets.get(i) + "; ") ;
+	    }
+	    bufferedWriter.write(nomsObjets.get(i) + "\n");
+	    bufferedWriter.close();
+	} catch (IOException ex) {
+	    System.err.println("Une erreur est apparue lors de l'écriture des traces au format CSV dans le fichier " + nomFichierSortie + ".") ;
+	    System.exit(3);
+	}
+    }
+    
+    private void execCommande (String [] cmd) {
+	try {
+	    Process p = Runtime.getRuntime().exec(cmd);
+	    new Thread(() -> {
+		try {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream())) ;
+		    String line ;
+		    while((line = br.readLine()) != null)
+			System.out.println(line);
+		} catch (IOException ex) {
+		    Logger.getLogger(Simulateur.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	    }).start();
+	    p.waitFor() ;
+	} catch (IOException | InterruptedException ex) {
+	    Logger.getLogger(Simulateur.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	Simulateur simulateur = new Simulateur(new Configurateur()) ;
+	simulateur.getConfigurateur().lireObjets("lol");
+	simulateur.ecrireFormatCSV("lel");
+	simulateur.execCommande(new String[]{"cat","lel"});
+	simulateur.lireFormatOneEventByLine("file.txt");
+    }
+    
+}
